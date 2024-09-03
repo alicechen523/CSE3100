49b40db94c77961c28194dc6ac046f4d00560d48
Alice Chen <alice.chen@uconn.edu>
2024-09-03 03:52:00 +0000

    Add lab0.c

---
+++ lab0.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+
+    int n = 2;
+    int sum = 0;
+    while (n < 200)
+    {
+        sum += n;
+        n += 2;
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
